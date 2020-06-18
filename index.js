af1475f10b33d86ba14d8952e7e90aa7f93316d1
Simon Kotlinski <simon.kotlinski@gmail.com>
2020-06-19 00:35:17 +0200

    feature(endpoint): download bets

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const path = require('path');
 const pot_bot = require('pot-bot');
 const PORT = process.env.PORT || 5000;
 
-runPotBot = async function (api_key, game_type) {
+runPotBot = async function (api_key, no_of_lines, game_type) {
   const draw = await pot_bot.fetchCurrentDraw(api_key, game_type);
   console.log(`draw: ${JSON.stringify(draw, null, 2)}`);
-  return draw;
-}
+  return await pot_bot.analyzeCurrentDraw(game_type, no_of_lines);
+};
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .get('/', async (req, res) => {
-      const draw = await runPotBot(req.query.api_key, 'stryktipset');
-      res.json(draw);
+      const bets_file_ref = await runPotBot(req.query.api_key, req.query.no_of_lines, 'stryktipset');
+      res.sendFile(bets_file_ref);
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
