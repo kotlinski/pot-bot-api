56ff827dfd47cb4995e5935957a0fc2b25c8be5a
Simon Kotlinski <simon.kotlinski@gmail.com>
2020-06-19 01:17:32 +0200

    res-write

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ runPotBot = async function (api_key, no_of_lines, game_type) {
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .get('/', async (req, res) => {
+      res.write('starting script...\n\n');
       const bets_file_ref = await runPotBot(req.query.api_key, req.query.no_of_lines, 'stryktipset');
       console.log(`bets_file_ref: ${JSON.stringify(bets_file_ref, null, 2)}`);
       console.log(`__dirname + : ${JSON.stringify(__dirname, null, 2)}`);
