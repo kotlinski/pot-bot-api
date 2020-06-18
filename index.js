425a600f702ba82d623c122f2364f62c5e3dee10
Simon Kotlinski <simon.kotlinski@gmail.com>
2020-06-19 00:09:45 +0200

    feature(lib): log draw

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const pot_bot = require('pot-bot');
 const PORT = process.env.PORT || 5000;
 
 async function runPotBot(api_key, game_type) {
-  return await pot_bot.fetchCurrentDraw(api_key, game_type);
+  const draw =  await pot_bot.fetchCurrentDraw(api_key, game_type);
+  console.log(`draw: ${JSON.stringify(draw, null, 2)}`);
+  return draw;
 }
 
 express()
