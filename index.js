f6fc80ad771d9180f2a84288815ceb66648ff64d
Simon Kotlinski <simon.kotlinski@gmail.com>
2020-06-19 01:56:14 +0200

    fixes

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ express()
     .get('/', async (req, res) => {
       res.setHeader('Content-type', "application/octet-stream");
       res.setHeader('Content-disposition', 'attachment; filename=file.txt');
-//       res.write('starting script...\n\n');
-      const bets_file_ref = await runPotBot(req.query.api_key, req.query.no_of_lines, 'stryktipset');
-      console.log(`bets_file_ref: ${JSON.stringify(bets_file_ref, null, 2)}`);
-      var text="hello world";
-      res.send(text);
+      const bets_to_print = await runPotBot(req.query.api_key, req.query.no_of_lines, 'stryktipset');
+      res.send(bets_to_print);
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`)).setTimeout(500000);
