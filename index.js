f020cd2c8d3326be2b410dcc84eec8f9ca09b4b3
Simon Kotlinski <simon.kotlinski@gmail.com>
2020-06-19 00:13:28 +0200

    feature(lib): await draw, then respons

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const path = require('path');
 const pot_bot = require('pot-bot');
 const PORT = process.env.PORT || 5000;
 
-async function runPotBot(api_key, game_type) {
-  const draw =  await pot_bot.fetchCurrentDraw(api_key, game_type);
+runPotBot = async function (api_key, game_type) {
+  const draw = await pot_bot.fetchCurrentDraw(api_key, game_type);
   console.log(`draw: ${JSON.stringify(draw, null, 2)}`);
   return draw;
 }
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
-    .get('/', (req, res) => res.json(runPotBot(req.query.api_key, 'stryktipset')))
+    .get('/', async (req, res) => {
+      const draw = await runPotBot(req.query.api_key, 'stryktipset');
+      res.json(draw);
+    })
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
