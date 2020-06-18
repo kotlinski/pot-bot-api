ad5377fe9470a77eca44f49bc6f688e1a9bdb030
Simon Kotlinski <simon.kotlinski@gmail.com>
2020-06-19 00:56:48 +0200

    chore(timeout): grant extra seconds

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const pot_bot = require('pot-bot');
 const PORT = process.env.PORT || 5000;
+const timeout = require('connect-timeout');
+
 
 runPotBot = async function (api_key, no_of_lines, game_type) {
   const draw = await pot_bot.fetchCurrentDraw(api_key, game_type);
@@ -11,8 +13,10 @@ runPotBot = async function (api_key, no_of_lines, game_type) {
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
+    .use(timeout('60s'))
     .get('/', async (req, res) => {
       const bets_file_ref = await runPotBot(req.query.api_key, req.query.no_of_lines, 'stryktipset');
+      console.log(`bets_file_ref: ${JSON.stringify(bets_file_ref, null, 2)}`);
       res.sendFile(bets_file_ref);
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
